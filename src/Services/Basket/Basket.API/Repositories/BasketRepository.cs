d32beaa34b25eb7cea0b0edde481ac7275912dd8
MITESH JHA <cool.miteshjha@gmail.com>
2021-10-09 21:30:02 +0530

    Added Repository layer for redis

---
+++ src/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,41 @@
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _redisCache;
+
+        public BasketRepository(IDistributedCache distributedCache)
+        {
+            this._redisCache = distributedCache ?? throw new ArgumentNullException(nameof(_redisCache));
+        }
+        public async Task DeleteBasket(string userName)
+        {
+           await _redisCache.RemoveAsync(userName);
+        }
+
+        public async Task<ShoppingCart> GetBasket(string userName)
+        {
+            var basket = await _redisCache.GetStringAsync(userName);
+            
+            if (string.IsNullOrEmpty(basket))
+                return null;
+
+            // JsonConvert.DeserializeObject will convert basket into ShoppingCart structure.
+            return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+        }
+
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
+        {
+            await _redisCache.SetStringAsync(basket.UserName, JsonConvert.SerializeObject(basket));
+            return await GetBasket(basket.UserName);
+        }
+    }
+}
