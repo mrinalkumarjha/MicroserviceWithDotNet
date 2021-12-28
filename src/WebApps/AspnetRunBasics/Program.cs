71402edb4558d998bb9fc541398e56e6197a3573
Mrinal Kumar <Mrinal.Kumar@BritishCouncil.Org>
2021-12-28 12:46:24 +0530

    Refactored application to use ocelot gateway.

--- src/WebApps/AspnetRunBasics/Program.cs
+++ src/WebApps/AspnetRunBasics/Program.cs
@@ -1,10 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using AspnetRunBasics.Data;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -36,8 +31,8 @@ namespace AspnetRunBasics
 
                 try
                 {
-                    var aspnetRunContext = services.GetRequiredService<AspnetRunContext>();
-                    AspnetRunContextSeed.SeedAsync(aspnetRunContext, loggerFactory).Wait();
+                   // var aspnetRunContext = services.GetRequiredService<AspnetRunContext>();
+                    //AspnetRunContextSeed.SeedAsync(aspnetRunContext, loggerFactory).Wait();
                 }
                 catch (Exception exception)
                 {
